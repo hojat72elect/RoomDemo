43608f2229b3f20e3852d313f535eb86c751e3b9
mojan <hojat72elect@gmail.com>
2022-03-01 19:32:24 -0500

    1- created the "repository" access point between ViewModel and Model. 2- created the GUI of the app.

---
+++ app/src/main/java/ca/sudbury/hojat/roomdemo/db/SubscriberRepository.kt
@@ -0,0 +1,28 @@
+package ca.sudbury.hojat.roomdemo.db
+
+/**
+ * Created by Hojat Ghasemi at 2022-03-01
+ * Contact the author at "https://github.com/hojat72elect"
+ *
+ * The access-point between Model layer and ViewModel layer
+ */
+class SubscriberRepository(private val dao: SubscriberDAO) {
+
+    val subscribers = dao.getAllSubscribers()
+
+    suspend fun insert(subscriber: Subscriber) {
+        dao.insertSubscriber(subscriber)
+    }
+
+    suspend fun update(subscriber: Subscriber) {
+        dao.updateSubscriber(subscriber)
+    }
+
+    suspend fun delete(subscriber: Subscriber) {
+        dao.deleteSubscriber(subscriber)
+    }
+
+    suspend fun deleteAll() {
+        dao.deleteAll()
+    }
+}
